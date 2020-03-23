6191cc729b4b4d40dc25291d2d7678c14412c435
davey <d.hu-1@student.utwente.nl>
2020-03-23 10:32:00 +0100

    Added some socketio room functionality for creating connections

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, send_from_directory
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit, join_room, leave_room
 app = Flask(__name__)
 
 socketio = SocketIO(app)
@@ -11,6 +11,24 @@ def send_static(path):
         mimetype = 'text/javascript'
     return send_from_directory('static', path, mimetype=mimetype)
 
+@socketio.on('join')
+def on_join(data):
+    #username = data['username']
+    channel = data['channel']
+    join_room(channel)
+    #send(username + ' has entered the room.', channel=channel)
+
+@socketio.on('leave')
+def on_leave(data):
+    #username = data['username']
+    room = data['room']
+    leave_room(room)
+    #send(username + ' has left the room.', room=room)
+
+@socketio.on("send message")
+def message(data):
+    room = data['channel']
+    emit('broadcast message', data['message'], room=room)
 
 @app.route('/')
 def index():
