e5a759872b65eeac407a8b9b76fb321b5b5204a4
JvdK <j.vanderkaaij@student.utwente.nl>
2020-04-03 09:42:00 +0200

    Add cards

--- frontend/src/gui.js
+++ frontend/src/gui.js
@@ -19,12 +19,20 @@ export async function draw_loop() {
     canvas.width  = window.innerWidth;
     canvas.height = window.innerHeight;
     ctx = canvas.getContext("2d");
+
+    // var imageObj = new Image();
+    // imageObj.src = '/static/images/Diamond 2.png';
+
+    var imageObjA = new Image();
+    var imageObjB = new Image();
+
     while(true) {
         await sleep(50);
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         switch(game.state) {
             case State.LOBBY:
                 draw_text('Waiting in lobby for other player. Calculating prime...');
+                // ctx.drawImage(imageObj, 0, 0, 200, 400);
                 break;
             case State.TABLE:
                 draw_text('Other player found. Preparing game...');
@@ -48,6 +56,11 @@ export async function draw_loop() {
         if (game.hand.length !== 0) {
             draw_text(`Your hand: ${game.get_card_value(game.hand[0])}, ${game.get_card_value(game.hand[1])}`,
                 canvas.width/2, canvas.height/1.5);
+
+            imageObjA.src = '/static/images/' + game.get_card_value(game.hand[0]) + '.png';
+            imageObjB.src = '/static/images/' + game.get_card_value(game.hand[1]) + '.png';
+            ctx.drawImage(imageObjA, canvas.width / 2 - 100, canvas.height-200, 130, 200);
+            ctx.drawImage(imageObjB, canvas.width / 2 + 100, canvas.height-200, 130, 200);
         }
         if (game.river.length !== 0) {
             let river = game.river.map(x => game.get_card_value(x));
