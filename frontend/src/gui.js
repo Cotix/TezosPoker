55b01a3ed073feb1043cdb97c5c35439fbae5913
JvdK <j.vanderkaaij@student.utwente.nl>
2020-04-03 10:37:14 +0200

    Add River

--- frontend/src/gui.js
+++ frontend/src/gui.js
@@ -10,6 +10,13 @@ function draw_text(text, x, y) {
     ctx.fillText(text, x, y);
 }
 
+function draw_text2(text, x, y) {
+    if (x === undefined) x = canvas.width/2;
+    if (y === undefined) y = canvas.height/2 - 220;
+    ctx.textAlign = "center";
+    ctx.fillText(text, x, y);
+}
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -25,6 +32,13 @@ export async function draw_loop() {
 
     var imageObjA = new Image();
     var imageObjB = new Image();
+    var imageObjC = new Image();
+
+    var River1 = new Image();
+    var River2 = new Image();
+    var River3 = new Image();
+    var River4 = new Image();
+    var River5 = new Image();
 
     while(true) {
         await sleep(50);
@@ -47,10 +61,10 @@ export async function draw_loop() {
                 draw_text('Deck negotiated. Drawing cards..');
                 break;
             case State.BET0:
-                draw_text('Player 1\'s turn');
+                draw_text2('Player 1\'s turn');
                 break;
             case State.BET1:
-                draw_text('Player 2\'s turn');
+                draw_text2('Player 2\'s turn');
                 break;
         }
         if (game.hand.length !== 0) {
@@ -59,13 +73,56 @@ export async function draw_loop() {
 
             imageObjA.src = '/static/images/' + game.get_card_value(game.hand[0]) + '.png';
             imageObjB.src = '/static/images/' + game.get_card_value(game.hand[1]) + '.png';
-            ctx.drawImage(imageObjA, canvas.width / 2 - 100, canvas.height-200, 130, 200);
-            ctx.drawImage(imageObjB, canvas.width / 2 + 100, canvas.height-200, 130, 200);
+            ctx.drawImage(imageObjA, canvas.width / 2 - 75 - 65, canvas.height-200, 130, 200);
+            ctx.drawImage(imageObjB, canvas.width / 2 + 75 - 65, canvas.height-200, 130, 200);
+
+            imageObjC.src = '/static/images/BACK.png';
+            ctx.drawImage(imageObjC, canvas.width / 2 - 75 - 65, 0, 130, 200);
+            ctx.drawImage(imageObjC, canvas.width / 2 + 75 - 65, 0, 130, 200);
         }
         if (game.river.length !== 0) {
             let river = game.river.map(x => game.get_card_value(x));
             draw_text(`River: ${river.join(', ')}`,
                 canvas.width/2, canvas.height/1.75);
+
+            if(game.river.length === 3) {
+                River1.src = '/static/images/' + game.get_card_value(game.river[0]) + '.png';
+                River2.src = '/static/images/' + game.get_card_value(game.river[1]) + '.png';
+                River3.src = '/static/images/' + game.get_card_value(game.river[2]) + '.png';
+                River4.src = '/static/images/BACK.png';
+                River5.src = '/static/images/BACK.png';
+                ctx.drawImage(River1, canvas.width / 2 - 65 - 130 - 130 - 10 - 10, canvas.height/2 -100, 130, 200);
+                ctx.drawImage(River2, canvas.width / 2 - 65 - 130 - 10, canvas.height/2 -100, 130, 200);
+                ctx.drawImage(River3, canvas.width / 2 - 65, canvas.height/2 -100, 130, 200);
+                ctx.drawImage(River4, canvas.width / 2 + 65 + 10, canvas.height/2 -100, 130, 200);
+                ctx.drawImage(River5, canvas.width / 2 + 65 + 130 + 10 + 10, canvas.height/2 -100, 130, 200);
+            }
+
+            if(game.river.length === 4) {
+                River1.src = '/static/images/' + game.get_card_value(game.river[0]) + '.png';
+                River2.src = '/static/images/' + game.get_card_value(game.river[1]) + '.png';
+                River3.src = '/static/images/' + game.get_card_value(game.river[2]) + '.png';
+                River4.src = '/static/images/' + game.get_card_value(game.river[3]) + '.png';
+                River5.src = '/static/images/BACK.png';
+                ctx.drawImage(River1, canvas.width / 2 - 65 - 130 - 130 - 10 - 10, canvas.height/2 -100, 130, 200);
+                ctx.drawImage(River2, canvas.width / 2 - 65 - 130 - 10, canvas.height/2 -100, 130, 200);
+                ctx.drawImage(River3, canvas.width / 2 - 65, canvas.height/2 -100, 130, 200);
+                ctx.drawImage(River4, canvas.width / 2 + 65 + 10, canvas.height/2 -100, 130, 200);
+                ctx.drawImage(River5, canvas.width / 2 + 65 + 130 + 10 + 10, canvas.height/2 -100, 130, 200);
+            }
+
+            if(game.river.length === 5) {
+                River1.src = '/static/images/' + game.get_card_value(game.river[0]) + '.png';
+                River2.src = '/static/images/' + game.get_card_value(game.river[1]) + '.png';
+                River3.src = '/static/images/' + game.get_card_value(game.river[2]) + '.png';
+                River4.src = '/static/images/' + game.get_card_value(game.river[3]) + '.png';
+                River5.src = '/static/images/' + game.get_card_value(game.river[4]) + '.png';
+                ctx.drawImage(River1, canvas.width / 2 - 65 - 130 - 130 - 10 - 10, canvas.height/2 -100, 130, 200);
+                ctx.drawImage(River2, canvas.width / 2 - 65 - 130 - 10, canvas.height/2 -100, 130, 200);
+                ctx.drawImage(River3, canvas.width / 2 - 65, canvas.height/2 -100, 130, 200);
+                ctx.drawImage(River4, canvas.width / 2 + 65 + 10, canvas.height/2 -100, 130, 200);
+                ctx.drawImage(River5, canvas.width / 2 + 65 + 130 + 10 + 10, canvas.height/2 -100, 130, 200);
+            }
         }
 
     }
